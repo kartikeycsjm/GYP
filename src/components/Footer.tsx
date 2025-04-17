4ef0aa3b1c3dcb8dcbedc12f2c58a22c999b1e80
Kartikey Mishra <kartikeymishracsjm@gmail.com>
2025-04-17 22:10:26 +0530

    added some links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,8 @@ const Footer = () => {
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
             <div className="flex items-center gap-2">
                 <Briefcase className="h-5 w-5" />
-                <p className="text-xs text-gray-500">© {new Date().getFullYear()} PortfolioGen. All rights reserved.</p>
+                <p className="text-xs text-gray-500">© {new Date().getFullYear()} PortfolioGen. All rights reserved. Created by Kartikey Mishra
+                </p>
             </div>
             <nav className="sm:ml-auto flex gap-4 sm:gap-6">
                 <Link href="/terms" className="text-xs hover:underline underline-offset-4">
@@ -16,8 +17,8 @@ const Footer = () => {
                 <Link href="/privacy" className="text-xs hover:underline underline-offset-4">
                     Privacy Policy
                 </Link>
-                <Link href="/contact" className="text-xs hover:underline underline-offset-4">
-                    Contact Us
+                <Link href="https://kartikeymishra.vercel.app/contact" className="text-xs hover:underline underline-offset-4">
+                    Contact Me
                 </Link>
             </nav>
         </footer>
